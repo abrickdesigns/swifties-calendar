26737333bd42151708fcfe11f52eedd91e9def5c
Amit Brickman <amitbrickman@Amits-MacBook-Pro.local>
2023-07-19 18:59:52 +0300

    full year counting fix, desktop style

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,7 +1,7 @@
 export const daysBetween = (firstDate: Date, secondDate: Date) => {
     const ONE_DAY = 24 * 60 * 60 * 1000;
-
     const differenceMs = Math.abs(secondDate.getTime() - firstDate.getTime());
+    const fullYearAddition = secondDate.getFullYear() === 2024 && secondDate.getMonth()> 1 ? 1 : 0;
 
-    return Math.round(differenceMs / ONE_DAY);
+    return Math.round(differenceMs / ONE_DAY) + fullYearAddition;
 }
